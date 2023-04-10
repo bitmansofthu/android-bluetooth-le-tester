167336df1ce0ecac0841440163334cf7b1db91a6
tikanpet <tikanderp@gmail.com>
2023-04-10 15:51:06 +0300

    Added expanded list (instead of Recycler view) view for showing  services and characteristics.

--- app/src/main/java/com/example/ble_test_v13_0/MainActivity.java
+++ app/src/main/java/com/example/ble_test_v13_0/MainActivity.java
@@ -11,7 +11,9 @@ import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCallback;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothProfile;
+import android.database.DataSetObserver;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Handler;
 
 import android.content.Context;
@@ -30,6 +32,8 @@ import android.bluetooth.le.BluetoothLeScanner;
 import android.bluetooth.le.ScanCallback;
 import android.bluetooth.le.ScanResult;
 
+import android.widget.ExpandableListAdapter;
+import android.widget.ExpandableListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -82,6 +86,9 @@ public class MainActivity extends AppCompatActivity {
 // TODO: move to some proper place
     private RecyclerView servicesRecyclerView;
     private RecyclerView.Adapter servicesAdapter;
+
+    private ExpandableListView servicesExpandableListView;
+    private ExpandableListAdapter expandableServicesAdapter;
     ArrayList<ServiceModel> serviceModelArrayList = new ArrayList<>();
     ArrayList<ArrayList<CharacteristicsModel>> characteristicsModelArrayList =
             new ArrayList<ArrayList<CharacteristicsModel>>();
@@ -492,73 +499,154 @@ public class MainActivity extends AppCompatActivity {
         }
 
         serviceModelArrayList.add(new ServiceModel("PROFILE 2", "2345-5678"));
-        characteristicsModelArrayList.add(null);
-
-        serviceModelArrayList.add(new ServiceModel("PROFILE 3", "3456-6789"));
         {
             ArrayList<CharacteristicsModel> CharPerServiceArrayList =
+                    new ArrayList<CharacteristicsModel>();
                     new ArrayList<CharacteristicsModel>();
             CharPerServiceArrayList.add(new CharacteristicsModel("12345678"));
             CharPerServiceArrayList.add(new CharacteristicsModel("87654321"));
             characteristicsModelArrayList.add(CharPerServiceArrayList);
         }
-        serviceModelArrayList.add(new ServiceModel("PROFILE 4", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 5", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 6", "3456-6789"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 7", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 8", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 9", "3456-6789"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 10", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 11", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 12", "3456-6789"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 13", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 14", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 15", "3456-6789"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 16", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 17", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 18", "3456-6789"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 19", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 20", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 21", "3456-6789"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 22", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 23", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 24", "3456-6789"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 25", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 26", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 27", "3456-6789"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 28", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 29", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 30", "3456-6789"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 31", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 32", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 33", "3456-6789"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 34", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 35", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 36", "3456-6789"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 37", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 38", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 39", "3456-6789"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 40", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 41", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 42", "3456-6789"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 43", "1234-4567"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 44", "2345-5678"));
-        serviceModelArrayList.add(new ServiceModel("PROFILE 45", "3456-6789"));
 
-        for (int i=0; i<serviceModelArrayList.size(); i++)
+        serviceModelArrayList.add(new ServiceModel("PROFILE 3", "3456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 4", "4234-4567"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 5", "5345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 6", "6456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 7", "7234-4567"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 8", "8345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 9", "9456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 10", "10234-4567"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 11", "11345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 12", "12456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 13", "13234-4567"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 14", "14345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 15", "15456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 16", "16234-4567"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 17", "17345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 18", "18456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 19", "19234-4567"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 20", "20345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 21", "21456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 22", "22234-4567"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 23", "23345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 24", "24456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 25", "25234-4567"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 26", "26345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 27", "27456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 28", "28234-4567"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 29", "29345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 30", "30456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 31", "31234-4567"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 32", "32345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 33", "3456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 34", "34234-4567"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 35", "35345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 36", "36456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 37", "37234-4567"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 38", "38345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 39", "39456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 40", "40234-4567"));
         {
-            characteristicsItemEnabledList.add(false);
+            ArrayList<CharacteristicsModel> CharPerServiceArrayList =
+                    new ArrayList<CharacteristicsModel>();
+            new ArrayList<CharacteristicsModel>();
+            CharPerServiceArrayList.add(new CharacteristicsModel("97654321"));
+            CharPerServiceArrayList.add(new CharacteristicsModel("82345678"));
+            CharPerServiceArrayList.add(new CharacteristicsModel("77654321"));
+            CharPerServiceArrayList.add(new CharacteristicsModel("62345678********"));
+            CharPerServiceArrayList.add(new CharacteristicsModel("57654321----------"));
+            characteristicsModelArrayList.add(CharPerServiceArrayList);
         }
 
-        showGattProfilesInRecyclerView();
+        serviceModelArrayList.add(new ServiceModel("PROFILE 41", "41345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 42", "42456-6789"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 43", "43234-4567"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 44", "44345-5678"));
+            characteristicsModelArrayList.add(null);
+        serviceModelArrayList.add(new ServiceModel("PROFILE 45", "45456-6789"));
+            characteristicsModelArrayList.add(null);
+
+/*        for (int i=0; i<serviceModelArrayList.size(); i++)
+        {
+            characteristicsItemEnabledList.add(false);
+        }*/
+
+        showGattProfilesInExpandableListView();
     }
 
+    public void showGattProfilesInExpandableListView(){
+        servicesExpandableListView = (ExpandableListView)findViewById(R.id.Services_expandableListView);
+
+        // Show services in Expandable type of List view (characteristics expanded)
+        expandableServicesAdapter = new ServicesExpandableListAdapter(MainActivity.this,
+                serviceModelArrayList, characteristicsModelArrayList);
+        servicesExpandableListView.setAdapter(expandableServicesAdapter);
+
+        expandableServicesAdapter.registerDataSetObserver(new DataSetObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+            }
+        });
+
+        servicesExpandableListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getApplicationContext(),
+                        serviceModelArrayList.get(groupPosition) + " List Expanded.",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /*
     private void showGattProfilesInRecyclerView(){
         // Show services in RecyclerView type of List view:
         // One Parent RecyclerView (RV) for services
         // Child RVs for each characteristics (created inside BTServicesRecyclerViewAdapter)
         servicesRecyclerView = findViewById(R.id.Services_recyclerView);
-        servicesRecyclerView.setHasFixedSize(false);
+        servicesRecyclerView.setHasFixedSize(true);
 
         serviceLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
         servicesAdapter = new BTServicesRecyclerViewAdapter(serviceModelArrayList, MainActivity.this);
@@ -572,4 +660,5 @@ public class MainActivity extends AppCompatActivity {
 
         servicesAdapter.notifyDataSetChanged();
     }
+    */
 }
---
+++ app/src/main/java/com/example/ble_test_v13_0/ServicesExpandableListAdapter.java
@@ -0,0 +1,109 @@
+package com.example.ble_test_v13_0;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class ServicesExpandableListAdapter extends BaseExpandableListAdapter {
+    private Context context;
+    private ArrayList<MainActivity.ServiceModel> groupArrayList;
+    private ArrayList<ArrayList<MainActivity.CharacteristicsModel>> childArrayList;
+
+    public ServicesExpandableListAdapter(Context context, ArrayList<MainActivity.ServiceModel> groupArrayList,
+                                         ArrayList<ArrayList<MainActivity.CharacteristicsModel>> childArrayList) {
+        this.context = context;
+        this.groupArrayList = groupArrayList;
+        this.childArrayList = childArrayList;
+    }
+    @Override
+    public int getGroupCount() {
+        return this.groupArrayList.size();
+    }
+
+    @Override
+    public int getChildrenCount(int groupPosition) {
+        if (this.childArrayList.get(groupPosition) != null){
+            return this.childArrayList.get(groupPosition).size();
+        }
+        else {
+            return 0;
+        }
+    }
+
+    @Override
+    public Object getGroup(int groupPosition) {
+        return this.groupArrayList.get(groupPosition);
+    }
+
+    @Override
+    public Object getChild(int groupPosition, int childPosition) {
+        return this.childArrayList.get(groupPosition).get(childPosition);
+    }
+
+    @Override
+    public long getGroupId(int groupPosition) {
+        return groupPosition;
+    }
+
+    @Override
+    public long getChildId(int groupPosition, int childPosition) {
+        return childPosition;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return false;
+    }
+
+    @Override
+    public View getGroupView(int groupPosition, boolean isExpanded,
+                             View convertView, ViewGroup parent) {
+        MainActivity.ServiceModel service = (MainActivity.ServiceModel) getGroup(groupPosition);
+        String service_name = service.getServiceName();
+        String service_uuid = service.getServiceUUID();
+
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context.
+                    getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.services_list_group, null);
+        }
+        TextView ServiceNameTextListView = (TextView) convertView
+                .findViewById(R.id.service_group_name);
+        ServiceNameTextListView.setTypeface(null, Typeface.BOLD);
+        ServiceNameTextListView.setText(service_name);
+        TextView ServiceUuidListTextView = (TextView) convertView
+                .findViewById(R.id.service_group_id);
+        ServiceUuidListTextView.setTypeface(null, Typeface.BOLD);
+        ServiceUuidListTextView.setText(service_uuid);
+
+        return convertView;
+    }
+
+    @Override
+    public View getChildView(int groupPosition, int childPosition, boolean isLastChild,
+                             View convertView, ViewGroup parent) {
+        MainActivity.CharacteristicsModel characteristic = (MainActivity.CharacteristicsModel)getChild(groupPosition, childPosition);
+
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater) this.context
+                    .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.list_item_view, null);
+        }
+        TextView CharUuidExpandedView = (TextView) convertView
+                .findViewById(R.id.expanded_characteristic_list_item);
+        CharUuidExpandedView.setText(characteristic.getCharacteristicsUUID());
+
+        return convertView;
+    }
+
+    @Override
+    public boolean isChildSelectable(int groupPosition, int childPosition) {
+        return false;
+    }
+}
